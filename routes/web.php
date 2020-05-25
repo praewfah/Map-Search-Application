5f18de2d2c52ae105c38e219361e54b571ee8781
Praew <praew@common-services.com>
2020-05-25 11:29:51 +0700

    Public Application

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,3 @@ Use App\Map;
 use Illuminate\Http\Request;
 
 Route::view('/{path?}', 'app');
-
-//Route::group(['middleware' => ['web']], function () {
-//    Route::get('/', 'Controller@index');
-//});
